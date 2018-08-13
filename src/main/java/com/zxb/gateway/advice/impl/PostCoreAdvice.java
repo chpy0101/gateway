ce926063f786d939d3805e6bd084eb4a90a6833a
chen <>
2018-08-13 11:48:38 +0800

    post bug 修复

--- src/main/java/com/zxb/gateway/advice/impl/PostCoreAdvice.java
+++ src/main/java/com/zxb/gateway/advice/impl/PostCoreAdvice.java
@@ -14,31 +14,30 @@ public class PostCoreAdvice implements CoreAdvice {
     public String proxyTransRequest(String proxyUrl, GatewayRequest body, HttpServletRequest servlet) throws Exception {
 
         String contentType = servlet.getContentType();
-        proxyUrl += ("?" + servlet.getQueryString());
+        proxyUrl += ("?" + servlet.getQueryString() == null ? "" : servlet.getQueryString());
 
-        //json无改动直接透传。避免序列化最优性能
-        if (body.getJsonContentByte() != null) {
-            return HttpHelper.doPost(proxyUrl, contentType, body.getJsonContentByte());
-        }
         //有前置处理器时
         if (body.getMapParams() != null) {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (Map.Entry entry : body.getMapParams().entrySet()) {
+                stringBuilder.append(entry.getKey());
+                stringBuilder.append("=");
+                stringBuilder.append(entry.getValue());
+                stringBuilder.append("&");
+            }
+            stringBuilder.deleteCharAt(stringBuilder.length() - 1);
             //表单类型
             if (contentType.equals("application/x-www-form-urlencoded")) {
-                StringBuilder stringBuilder = new StringBuilder();
-                for (Map.Entry entry : body.getMapParams().entrySet()) {
-                    stringBuilder.append(entry.getKey());
-                    stringBuilder.append("=");
-                    stringBuilder.append(entry.getValue());
-                    stringBuilder.append("&");
-                }
-                if (body != null && ((Map) body).size() > 0)
-                    stringBuilder.deleteCharAt(stringBuilder.length() - 1);
                 return HttpHelper.doPost(proxyUrl, contentType, stringBuilder.toString().getBytes(utf8));
             }
             //json格式
             else {
-                return HttpHelper.doPost(proxyUrl, contentType, JsonMapper.instance().writeValueAsBytes(body));
+                return HttpHelper.doPost(proxyUrl + stringBuilder.toString(), contentType, body.getJsonContentByte());
             }
+        }
+        //json无改动直接透传。避免序列化最优性能
+        if (body.getJsonContentByte() != null) {
+            return HttpHelper.doPost(proxyUrl, contentType, body.getJsonContentByte());
         }
         return "";
     }
